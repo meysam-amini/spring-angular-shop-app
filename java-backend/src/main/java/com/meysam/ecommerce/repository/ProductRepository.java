b0746931d064ed2e0af6289a24071663329b459f
meysam <meysamamini7394@gmail.com>
2021-08-16 23:21:57 +0430

    readme updated

--- java-backend/src/main/java/com/meysam/ecommerce/repository/ProductRepository.java
+++ java-backend/src/main/java/com/meysam/ecommerce/repository/ProductRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@CrossOrigin(origins = "http://localhost:4200")
+CrossOrigin(origins = "http://127.0.0.1:4200")
 public interface ProductRepository extends JpaRepository<Product,Long> {
 
     Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable p);
